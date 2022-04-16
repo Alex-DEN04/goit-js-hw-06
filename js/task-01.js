04453c37c73ceb67dca27d715b73cc364661a89e
Oleksandr Levchuk <emihrant.ua@gmail.com>
2022-04-16 16:52:47 +0200

    task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,1 +1,15 @@
-const 
+const categoriesEl = document.querySelectorAll('.item');
+
+const countNumberCategories = el => {
+    return `Number of categories: ${el.length}`
+};
+console.log(countNumberCategories(categoriesEl));
+
+const res = el => {
+    const countNumberEl = el.lastElementChild.children.length;
+    const showTitleText = el.firstElementChild.textContent;
+    console.log(`Category: ${showTitleText}`)
+    console.log(`Elements: ${countNumberEl}`)
+};
+
+const result = [...categoriesEl].map(item => (res(item)));
