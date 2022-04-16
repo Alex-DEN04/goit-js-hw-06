a43b2892a2e9c2b887a9bd73899d598b85d25b5c
Oleksandr Levchuk <emihrant.ua@gmail.com>
2022-04-16 18:26:44 +0200

    task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,17 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+const ingredientsList = document.querySelector('#ingredients');
+const itemArray = [];
+
+const foo = (array) => array.map(item => {
+  const itemEl = document.createElement('li');
+  itemEl.textContent = item;
+  itemEl.classList = 'item';
+  return itemArray.push(itemEl);
+});
+
+foo(ingredients);
+
+const addItems = ingredientsList.prepend(...itemArray);
+
