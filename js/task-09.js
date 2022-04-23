ed7868fb857b3c22e85ebb898b21fa013c614925
Oleksandr Levchuk <emihrant.ua@gmail.com>
2022-04-23 20:12:10 +0200

    task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,17 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+const refs = {
+  body: document.querySelector('body'),
+  button: document.querySelector('.change-color'),
+  color: document.querySelector('.color'),
 }
+
+refs.button.addEventListener('click', onClickHandler);
+
+function onClickHandler() {
+  refs.body.style.backgroundColor = getRandomHexColor();
+  refs.color.textContent = refs.body.style.backgroundColor;
+}
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+
