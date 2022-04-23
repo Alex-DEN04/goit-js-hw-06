4d097d4fef321e6f3687d7e8ec5c9e376f678307
Oleksandr Levchuk <emihrant.ua@gmail.com>
2022-04-23 21:00:52 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -3,14 +3,16 @@ const formEl = document.querySelector(".login-form");
 formEl.addEventListener("submit", onSubmitHandler);
 
 function onSubmitHandler(event) {
+  event.preventDefault();
   
   const email = formEl.elements.email.value;
   const password = formEl.elements.password.value;
-    
+
   if (email === "" || password === "") {
     alert("Please fill in all the fields!");
     return 
   }
+ 
   const formData = new FormData(event.currentTarget);
   console.log(formData);
   
@@ -19,8 +21,7 @@ function onSubmitHandler(event) {
   for (const el of formData) {
     formDataObj[el[0]] = el[1];
   }
+
   console.dir(formDataObj);
   event.currentTarget.reset();
-  event.preventDefault();
 }
-onSubmitHandler(formEl);
